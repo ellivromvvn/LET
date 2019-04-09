61ed3aeb87fef5bc665f7db1aa0ee735159d0902
Orville Hombrebueno <el_livro3485@yahoo.com>
2019-04-09 16:49:42 +0800

    fin data and analysis

--- ndata_analysis.R
+++ ndata_analysis.R
@@ -14,9 +14,43 @@ plot(ent_let_percentile_data$LET_Results,ent_let_percentile_data$Nonverbal_Abili
 
 #logit regression 
 
-glm.fit<-glm(LET_Results~Vocabulary+Nonverbal_Ability,data=ent_let_percentile_data,
-             family = binomial)
-glm.fit2<-glm(LET_Results~Vocabulary+Nonverbal_Ability+Analogy+Numerical_Ability,data=ent_let_percentile_data,
+glm.fit<-glm(LET_Results~Vocabulary+Nonverbal_Ability+Analogy+Numerical_Ability,data=ent_let_percentile_data,
               family = binomial)
-glm.fit3<-glm(LET_Results~Vocabulary+Nonverbal_Ability+Analogy,data=ent_let_percentile_data,
+glm.fit2<-glm(LET_Results~Vocabulary+Nonverbal_Ability+Numerical_Ability,data=ent_let_percentile_data,
               family = binomial)
+glm.fit3<-glm(LET_Results~Vocabulary+Nonverbal_Ability,data=ent_let_percentile_data,
+              family = binomial)
+glm.fit4<-glm(LET_Results~Nonverbal_Ability,data=ent_let_percentile_data,
+              family = binomial)
+
+summary(glm.fit4)
+coef(glm.fit3)
+
+glm.probs<-predict(glm.fit2,type="response")
+glm.probs
+
+plot(glm.probs)
+
+glm.pred<-rep("failed",97)
+glm.pred[glm.probs>=0.5]<-"passed"
+table(glm.pred,ent_let_percentile_data$LET_Results)
+
+#correct classification rate
+mean(glm.pred==ent_let_percentile_data$LET_Results)
+
+#predicting with new data
+
+Vocabulary<-39
+Analogy<-32
+Numerical_Ability<-70
+Nonverbal_Ability<-50
+a<-data.frame(Vocabulary,Analogy,Numerical_Ability,Nonverbal_Ability)
+
+predict(glm.fit2,newdata = a, type = "response")
+
+Vocabulary<-39
+Numerical_Ability<-70
+Nonverbal_Ability<-50
+b<-data.frame(Vocabulary,Numerical_Ability,Nonverbal_Ability)
+
+predict(glm.fit2,newdata = b, type = "response")
